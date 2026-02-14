da5548d3053c42ff195baf1adbae1a09e6394fa8
malavikakan <kancherlamalavika@gmail.com>
2026-02-14 10:24:18 +0530

    Create design.sv

---
+++ Exp2/design.sv
@@ -0,0 +1,18 @@
+//---------------------------------
+// 2:1 Multiplexer Design
+//---------------------------------
+module mux2to1 (
+    input  logic a,
+    input  logic b,
+    input  logic sel,
+    output logic y
+);
+
+    always_comb begin
+        if (sel)
+            y = b;
+        else
+            y = a;
+    end
+
+endmodule
