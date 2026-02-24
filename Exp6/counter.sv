97e43c9ad769093d291e895c85ac2e5d3c18d6c7
malavikakan <kancherlamalavika@gmail.com>
2026-02-24 22:29:11 +0530

    Create counter.sv

---
+++ Exp6/counter.sv
@@ -0,0 +1,15 @@
+// 4-bit Up Counter (Sync Reset)
+module counter (
+    input  logic clk,
+    input  logic rst,
+    output logic [3:0] count
+);
+
+    always_ff @(posedge clk) begin
+        if (rst)
+            count <= 4'd0;
+        else
+            count <= count + 1;
+    end
+
+endmodule
